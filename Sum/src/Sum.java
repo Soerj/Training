4841bae8428ce01e89a8ffe310bce2b65257fbcd
Soerj <shaman32r@gmail.com>
2019-10-09 10:33:06 +0300

    recursion

--- Sum/src/Sum.java
+++ Sum/src/Sum.java
@@ -4,12 +4,17 @@ public class Sum
 
 	public static void main(String[] args)
 	{
-		int n = 150;
+		int n = 250;
 		System.out.println(count(n));
 	}
 
 	public static int count(int n)
 	{
+		if (n < 0)
+		{
+			System.out.println("Are you nuts?");
+			return 0;
+		}
 		if (n > 0)
 			return n + count(n - 1);
 		else
